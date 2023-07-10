5ea28d931128bd061a272571507592e37489b14b
Adam Hennessy <ah724@leicester.ac.uk>
2023-07-10 03:21:01 +0100

    get sprites

--- app_scripts/update_poketable.py
+++ app_scripts/update_poketable.py
@@ -4,22 +4,32 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, 
 ## Add a search for pokemon.json and update it.
 ## Source: supereffective.gg github table.
 
+base_sprite = 'static/sprites/'
+
 ####
 # LOAD JSON
 with open('archive/pokemon.json', 'r') as j:
     data = json.loads(j.read())
 
-columns_to_keep = ['id', 'nid', 'dexNum', 'formId', 'name', 'abilities']
+columns_to_keep = ['name', 'dexNum', 'refs', 'abilities', 'formId', 'isFemaleForm']
 
 filtered_data = [{column: record[column] for column in columns_to_keep if column in record} for record in data]
 
 for row in filtered_data:
     if row['abilities']['hidden'] == None:
         row['abilities'] = row['abilities']['primary']
-        row['has_hidden'] = False
+        row['hiddenAbility'] = False
     else:
         row['abilities'] = row['abilities']['hidden']
-        row['has_hidden'] = True
+        row['hiddenAbility'] = True
+    
+    row['smogon'] = row['refs']['smogon']
+    del row['refs']
+
+    row['sprite'] = base_sprite + row['smogon']
+
+    row['isFemale'] = row['isFemaleForm']
+    del row['isFemaleForm']
 
 ####
 # LOAD SQL
@@ -30,36 +40,31 @@ meta.create_all(engine)
 # Create table.
 pokemon_table = Table('pokemon', meta,
                     Column('internalId', Integer, primary_key=True, autoincrement=True),
-                    Column('id', String),
-                    Column('nid', String),
-                    Column('dexNum', Integer),
                     Column('name', String),
+                    Column('dexNum', Integer),
                     Column('formId', String),
-                    Column('has_hidden', Boolean),
-                    Column('abilities', String))
+                    Column('smogon', String),
+                    Column('hiddenAbility', Boolean),
+                    Column('abilities', String),
+                    Column('sprite', String))
 
 pokemon_table.create(engine)
 
 # Load data.
 with engine.connect() as conn:
     for row in filtered_data:
-        insert_query = pokemon_table.insert().values(dexNum=row['dexNum'],
-                                                id=row['id'],
-                                                nid=row['nid'],
-                                                name=row['name'],
-                                                formId=row['formId'],
-                                                has_hidden=row['has_hidden'],
-                                                abilities=row['abilities'])
+        insert_query = pokemon_table.insert().values(
+                                    name = row['name'],
+                                    dexNum = row['dexNum'],
+                                    formId = row['formId'],
+                                    smogon = row['smogon'],
+                                    hiddenAbility = row['hiddenAbility'],
+                                    abilities = row['abilities'],
+                                    sprite = row['sprite']
+                                    )
         conn.execute(insert_query)
     
     conn.commit()
     conn.close()
 
 print("Success rewriting table.")
-
-
-# next create engine and make into sql db
-# then create search form
-# as user type it searches through db for name
-# can show name + sprite
-# if user select then add this row to db
